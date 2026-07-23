024a74ecdee968ce42e8d339ab97e9ef6e2dc1b2
sarthaknirula <sarthaknirula13@gmail.com>
2026-07-23 14:58:15 +0530

    Creating service.py to connect with gemini-flash2.5.

---
+++ ai/service.py
@@ -0,0 +1,17 @@
+import os
+from google import genai
+from google.genai import types
+
+from prompts import SYSTEM_PROMPT
+
+print(os.environ.get('API_KEY'))
+class AIService() :
+    def __init__(self):
+        pass
+
+    def chat(self):
+        pass
+
+    def generate_response(self):
+        pass
+    
